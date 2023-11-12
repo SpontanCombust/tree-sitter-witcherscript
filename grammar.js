ec4b3465ec6a73b75a944b3638f2b69e2ae9cc6f
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-11-12 22:11:03 +0100

    expressions

--- grammar.js
+++ grammar.js
@@ -1,3 +1,29 @@
+// higher number == greater precedence
+// based on C++'s operator precedence
+// https://en.cppreference.com/w/cpp/language/operator_precedence
+const PREC = {
+  DEFAULT: 0,
+  ASSIGN: 1,
+  TERNARY: 2,
+  OR: 3,
+  AND: 4,
+  BIT_OR: 5,
+  BIT_AND: 6,
+  EQ: 7,
+  RELATION: 8,
+  SUM: 9,
+  DIFF: 9,
+  MULT: 10,
+  DIV: 10,
+  MODULO: 10,
+  NEW: 11,
+  UNARY: 11,
+  CAST: 11,
+  MEMBER: 12,
+  CALL: 12,
+  ARRAY: 12,
+};
+
 module.exports = grammar({
   name: 'witcherscript',
   
@@ -8,13 +34,160 @@ module.exports = grammar({
 
   rules: {
     module: $ => repeat(
-      $._literal
+      $.expr
     ),
 
 
-    // LITERALS =====================================================
+    // EXPRESSIONS ==================================================
 
-    _literal: $ => choice(
+    expr: $ => choice(
+      $.assign_op_expr,
+      $.ternary_cond_expr,
+      $.binary_op_expr,
+      $.new_expr,
+      $.unary_op_expr,
+      $.cast_expr,
+      $.member_expr,
+      $.call_expr,
+      $.array_expr,
+      $.nested_expr,
+      $.this_expr,
+      $.super_expr,
+      $.parent_expr,
+      $.virtual_parent_expr,
+      $.ident,
+      $.literal
+    ),
+
+
+    assign_op_expr: $ => choice(
+      ...['=', '+=', '-=', '*=', '/=', '%=']
+        .map((op) =>
+          prec.left(PREC.ASSIGN, seq(
+            field('left', $._assign_op_left),
+            field('op', op),
+            field('right', $.expr)
+          ))
+      )
+    ),
+
+    _assign_op_left: $ => choice(
+      $.ident,
+      $.call_expr,
+      $.member_expr,
+      $.array_expr,
+      $.nested_expr
+    ),
+
+    ternary_cond_expr: $ => prec.right(PREC.TERNARY, seq(
+      field('cond', $.expr),
+      '?',
+      field('expr_if_true', $.expr),
+      ':',
+      field('expr_if_false', $.expr)
+    )),
+
+
+    binary_op_expr: $ => choice(
+      ...[
+        ['||', PREC.OR],
+        ['&&', PREC.AND],
+        ['|', PREC.BIT_OR],
+        ['&', PREC.BIT_AND],
+        ['!=', PREC.EQ],
+        ['==', PREC.EQ],
+        ['>=', PREC.RELATION],
+        ['>', PREC.RELATION],
+        ['<=', PREC.RELATION],
+        ['<', PREC.RELATION],
+        ['-', PREC.DIFF],
+        ['+', PREC.SUM],
+        ['%', PREC.MODULO],
+        ['/', PREC.DIV],
+        ['*', PREC.MULT],
+      ].map(([op, precedence]) =>
+        prec.left(precedence, seq(
+          field('left', $.expr),
+          field('op', op),
+          field('right', $.expr)
+        ))
+      )
+    ),
+    
+
+    new_expr: $ => prec.right(PREC.NEW, seq(
+      'new',
+      field('class', $.ident),
+      'in',
+      field('lifetime_obj', $.expr)
+    )),
+
+    unary_op_expr: $ => prec.right(PREC.UNARY, seq(
+      field('op', choice('-', '!', '~')),
+      field('expr', $.expr)
+    )),
+
+    cast_expr: $ => prec.right(PREC.CAST, seq(
+      '(',
+      field('type', $.ident),
+      ')',
+      field('value', $.expr),
+    )),
+
+
+    member_expr: $ => prec.left(PREC.MEMBER, seq(
+      field('accessor', $.expr),
+      '.',
+      field('member', $.ident)
+    )),
+
+    call_expr: $ => prec.left(PREC.CALL, seq(
+      field('func', $.expr),
+      '(',
+      field('args', commaSepOpt($.expr)),
+      ')'
+    )),
+
+    array_expr: $ => prec.left(PREC.ARRAY, seq(
+      field('accessor', $.expr),
+      '[',
+      field('index', $.expr),
+      ']'
+    )),
+
+
+    nested_expr: $ => seq(
+      '(',
+      field('expr', $.expr),
+      ')'
+    ),
+
+
+
+    // TOKENS =======================================================
+
+    this_expr: $ => token(
+      'this'
+    ),
+
+    super_expr: $ => token(
+      'super'
+    ),
+
+    parent_expr: $ => token(
+      'parent'
+    ),
+
+    virtual_parent_expr: $ => token(
+      'virtual_parent'
+    ),
+
+    ident: $ => token(
+      /[_a-zA-Z][_a-zA-Z0-9]+/
+    ),
+
+
+    literal: $ => choice(
       $.literal_null,
       $.literal_float,
       $.literal_int,
@@ -69,7 +242,6 @@ module.exports = grammar({
       '\'',
     )),
 
-    // WHITESPACE ===================================================
 
     comment: $ => token(choice(
       seq('//', /(\\+(.|\r?\n)|[^\\\n])*/),
@@ -94,6 +266,10 @@ function commaSep1(rule) {
     
 function commaSep(rule) {
   return optional(commaSep1(rule))
+}
+
+function commaSepOpt(rule) {
+  return optional(commaSep1(optional(rule)))
 }
     
 function commaSepTrail(rule) {
